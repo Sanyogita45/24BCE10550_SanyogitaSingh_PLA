fd6e5cd6c68dbe7159bce2b86b3fe085a937ba63
Sanyogita Singh <sanyogita.24bce10550@vitbhopal.ac.in>
2026-09-18 10:36:19 +0530

    Create Day39 : 443A - Anton and Letters.java

---
+++ Day39 : 443A - Anton and Letters.java
@@ -0,0 +1,18 @@
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+ 
+        Set<Character> set = new HashSet<>();
+ 
+        for (char ch : s.toCharArray()) {
+            if (ch >= 'a' && ch <= 'z') {
+                set.add(ch);
+            }
+        }
+ 
+        System.out.println(set.size());
+    }
+}
